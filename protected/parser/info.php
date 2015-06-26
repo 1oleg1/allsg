c00dca4f3e3a285d0515182e05536027b4f4b8a6
1oleg1 <sehin.oleg@gmail.com>
2015-06-26 14:56:23 +0300

    regergeg

--- protected/parser/info.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo dirname(__FILE__) . '/';
-phpinfo();
-/*
-if ($_SERVER['REQUEST_URI'] == "/index.php") {
-    header("Location: http://" . $_SERVER['HTTP_HOST'] . "/", false, 301);
-    die();
-}
-*/
--- protected/views/games/show.php
+++
@@ -1,2 +0,0 @@
-<?php //страница с подробной информацией о выбранной игре ?>
-<?php $this->renderPartial('_full_desc', array('game'=>$model)); ?>
